6343fc527b3c85bb645bb66b2982d731823be403
devanshee1 <devanshee7763@gmail.com>
2025-04-05 14:55:43 +0530

    changes

---
+++ script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Create a dummy file
+touch activity.txt
+
+# Loop through dates
+for i in {1..10}
+do
+  echo "Commit $i" >> activity.txt
+  GIT_AUTHOR_DATE="2025-01-01$iT12:00:00" GIT_COMMITTER_DATE="2025-04-04$iT12:00:00" \
+  git add activity.txt
+  git commit -m "Fake commit $i"
+done
